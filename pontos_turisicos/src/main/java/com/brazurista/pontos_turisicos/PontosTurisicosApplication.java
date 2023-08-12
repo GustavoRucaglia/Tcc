0afa0698d501d99f487ae3d4b5fb11dc04bd0276
GustavoRucaglia <111381127+gustavo-Bozetti@users.noreply.github.com>
2023-08-12 19:05:12 -0300

    criando DEMO de pesquisa por restaurante

---
+++ pontos_turisicos/src/main/java/com/brazurista/pontos_turisicos/PontosTurisicosApplication.java
@@ -0,0 +1,13 @@
+package com.brazurista.pontos_turisicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosTurisicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontosTurisicosApplication.class, args);
+	}
+
+}
---
+++ pontos_turisicos/src/test/java/com/brazurista/pontos_turisicos/PontosTurisicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brazurista.pontos_turisicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosTurisicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
