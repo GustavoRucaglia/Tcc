b242bf09a2afc5fd2e099a1d6e45b2a756d6571c
Rayane Vieira <rayvieiraaa@gmail.com>
2023-08-29 10:04:12 -0300

    pronto mamis

---
+++ src/main/java/com/itb/tcc/brazurista/BrazuristaApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.brazurista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazuristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazuristaApplication.class, args);
+	}
+
+}
